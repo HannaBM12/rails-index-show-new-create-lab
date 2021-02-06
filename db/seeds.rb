4b050e3348f703f35228b9395b57a2a196f2914d
HannaBM12 <Hannabmulugeta@gmail.com>
2021-02-05 23:51:54 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Coupon.destroy_all
+
+c1 = Coupon.create(coupon_code: "2256", store: 'ABC')
+c2 = Coupon.create(coupon_code: "2257", store: 'Safeway')
+c3 = Coupon.create(coupon_code: "2258", store: 'Cosco')
+c4 = Coupon.create(coupon_code: "2259", store: 'Target')
+c5 = Coupon.create(coupon_code: "2260", store: 'H&M')
